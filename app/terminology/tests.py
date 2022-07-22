642ffabfdb3a7103c54c54085773d81ac6903067
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-07-22 23:58:13 +0000

    Add tests for terminology app

--- app/terminology/tests.py
+++ app/terminology/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import Term
 
 # Create your tests here.
+
+
+class TerminologyTest(TestCase):
+
+    def setUp(self):
+        term = Term.objects.create(
+            definition='CORS', description='CORS description')
+
+    def test_term_create_success(self):
+        term = Term.objects.first()
+
+        self.assertEqual(term.definition, 'CORS')
+        self.assertEqual(term.description, 'CORS description')
+
+    def test_term_update_success(self):
+        term = Term.objects.first()
+        term.definition = 'HTTP'
+        term.description = 'HTTP description'
+        term.save()
+
+        self.assertEqual(Term.objects.first().definition, 'HTTP')
+        self.assertEqual(Term.objects.first().description, 'HTTP description')
