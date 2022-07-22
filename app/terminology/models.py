8f1c9f365dc39d4ae3b38beb6233dee7f7c7391b
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-07-22 23:58:00 +0000

    Add model for terminology app

--- app/terminology/models.py
+++ app/terminology/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+
+class Term(models.Model):
+    definition = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.definition
+
+
+admin.site.register(Term)
