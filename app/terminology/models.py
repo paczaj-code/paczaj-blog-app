1b743d988c7bba27880ff8f7abe7067e9fee2f26
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-07-24 04:11:29 +0000

    Update Term model

--- app/terminology/models.py
+++ app/terminology/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 from django.contrib import admin
+from django.template.defaultfilters import slugify
 
 
 class Term(models.Model):
-    definition = models.CharField(max_length=255, unique=True)
-    description = models.TextField()
+    definition = models.CharField(max_length=255, unique=True, )
+    slug = models.SlugField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.definition
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.definition)
+        super().save(*args, **kwargs)
 
 
 admin.site.register(Term)
