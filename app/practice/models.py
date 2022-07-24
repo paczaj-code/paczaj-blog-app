c9ebc61d85a99328d90c90c2c3dbd69a4535efe8
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-07-24 09:16:25 +0000

    Refactor practice model

--- app/practice/models.py
+++ app/practice/models.py
@@ -1,35 +1,23 @@
 from django.db import models
-from blog.models import Post
+from blog.models import Category, Post
 from django.contrib.auth.models import User
-# Create your models here.
-
-# class Meta:
-#     db_table = ''
-#     managed = True
-#     verbose_name = 'ModelName'
-#     verbose_name_plural = 'ModelNames'
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    icon = models.CharField(max_length=255, null=True, blank=True)
-    is_published = models.BooleanField(default=True)
-
-    class Meta:
-        db_table = 'practice_category'
-        # managed = True
+from django.template.defaultfilters import slugify
 
 
 class Practice(models.Model):
     name = models.CharField(max_length=255)
-    exersise = models.TextField()
+    slug = models.SlugField(max_length=255, blank=True, null=True)
+    category = models.ForeignKey(Category,  on_delete=models.CASCADE)
+    exercise = models.TextField()
     solution = models.TextField()
     related_posts = models.ManyToManyField(Post)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
-    author = models.ForeignKey(
-        User, related_name='author', on_delete=models.SET_NULL, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     class Meta:
         db_table = 'practice_exercise'
@@ -39,8 +27,12 @@ class UserSolution(models.Model):
     solution = models.TextField()
     user = models.ForeignKey(User, related_name='user',
                              on_delete=models.SET_NULL,  blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
     practise = models.ForeignKey(
         'Practice', related_name='practise', on_delete=models.SET_NULL, blank=True, null=True)
+
+    is_public = models.BooleanField(default=False)
 
     class Meta:
         db_table = 'practice_user_solution'
