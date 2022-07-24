8cd51bf3260d550a3a8c106ae20921658c6b3c26
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-07-24 04:11:50 +0000

    Update tests fo Term model

--- app/terminology/tests.py
+++ app/terminology/tests.py
@@ -1,26 +1,49 @@
 from django.test import TestCase
 from .models import Term
+from freezegun import freeze_time
+import datetime
 
-# Create your tests here.
 
-
+@freeze_time("2022-01-14")
 class TerminologyTest(TestCase):
 
     def setUp(self):
         term = Term.objects.create(
-            definition='CORS', description='CORS description')
+            definition='Co to CORS', description='CORS description')
 
-    def test_term_create_success(self):
+    def test_term_was_created_successully(self):
+        self.assertEqual(Term.objects.first().definition, 'Co to CORS')
+        self.assertEqual(Term.objects.first().description, 'CORS description')
+        self.assertEqual(Term.objects.first().slug, 'co-to-cors')
+        self.assertEqual(
+            Term.objects.first().created_at, datetime.datetime(2022, 1, 14, 0, 0, tzinfo=datetime.timezone.utc))
+        self.assertEqual(
+            Term.objects.first().modified_at, datetime.datetime(2022, 1, 14, 0, 0, tzinfo=datetime.timezone.utc))
+
+    def test_create_term_with_missing_definition_raises_error(self):
+        with self.assertRaises(Exception):
+            Term.objects.create(
+                definition=None, description='CORS description')
+
+    def test_update_term_with_missing_definition_raises_error(self):
         term = Term.objects.first()
+        term.definition = None
+        with self.assertRaises(Exception):
+            term.save()
 
-        self.assertEqual(term.definition, 'CORS')
-        self.assertEqual(term.description, 'CORS description')
-
-    def test_term_update_success(self):
+    def test_term_update_successfully(self):
         term = Term.objects.first()
-        term.definition = 'HTTP'
+        term.definition = 'HTTP wyjaśnienie'
         term.description = 'HTTP description'
         term.save()
-
-        self.assertEqual(Term.objects.first().definition, 'HTTP')
+        self.assertEqual(Term.objects.first().definition, 'HTTP wyjaśnienie')
         self.assertEqual(Term.objects.first().description, 'HTTP description')
+        self.assertEqual(Term.objects.first().slug, 'http-wyjasnienie')
+        self.assertEqual(
+            term.created_at, datetime.datetime(2022, 1, 14, 0, 0, tzinfo=datetime.timezone.utc))
+        self.assertEqual(
+            term.modified_at, datetime.datetime(2022, 1, 14, 0, 0, tzinfo=datetime.timezone.utc))
+
+    def test_delete_term_successfully(self):
+        Term.objects.first().delete()
+        self.assertEqual(Term.objects.all().count(), 0)
