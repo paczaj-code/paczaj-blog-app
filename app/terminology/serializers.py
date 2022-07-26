66c01a9a4aba7412a0aa23c454f31efd781854f2
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-07-26 19:41:57 +0000

    Add serialzers for terminology app

---
+++ app/terminology/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Term
+
+
+class PublicTermListSerialzer(serializers.ModelSerializer):
+    class Meta:
+        model = Term
+        fields = ['id', 'definition',  'slug']
+
+
+class PublicTermDetailSerializer(PublicTermListSerialzer):
+    class Meta:
+        model = Term
+        fields = PublicTermListSerialzer.Meta.fields + ['description']
