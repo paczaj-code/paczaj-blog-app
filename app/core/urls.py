48ceb5593727046cee674120cf21a9d49f61021f
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-07-27 09:48:53 +0000

    Add path for blog API

--- app/core/urls.py
+++ app/core/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from terminology import urls
+from blog import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('terminology.urls')),
+    path('api/', include('blog.urls')),
 ]
