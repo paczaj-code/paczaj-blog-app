c83f76e58090fa9c966cafa18dfaeeb0c1c3cd4e
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-07-28 18:35:39 +0000

    Change blog model

--- app/blog/models.py
+++ app/blog/models.py
@@ -3,11 +3,22 @@ from distutils import text_file
 from django.db import models
 from django.contrib import admin
 from django.template.defaultfilters import slugify
-# from practice.models import Practice
 
 
 class Category(models.Model):
+    """Model for post categories"""
+
+    POST = 'P'
+    PRACTISE = 'E'
+    TERM = 'T'
+    CHOICES = (
+        (POST, "Post"),
+        (PRACTISE, "Practise"),
+        (TERM, "Terminology"),
+    )
     name = models.CharField(max_length=150, unique=True)
+    category_type = models.CharField(
+        max_length=1, choices=CHOICES, default=POST)
     icon = models.CharField(max_length=100, null=True, blank=True)
     slug = models.SlugField(max_length=255, blank=True, null=True)
     description = models.TextField(null=True, blank=True)
@@ -15,7 +26,7 @@ class Category(models.Model):
     modified_at = models.DateTimeField(auto_now=True)
     is_enabled = models.BooleanField(default=True)
     parent = models.ForeignKey(
-        'Category', related_name='parent_category', on_delete=models.SET_NULL, null=True, blank=True)
+        'Category', related_name='subcategories', on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Category'
@@ -25,8 +36,12 @@ class Category(models.Model):
         self.slug = slugify(self.name)
         super().save(*args, **kwargs)
 
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
+    """Model for post tags"""
     name = models.CharField(max_length=150, unique=True)
     slug = models.SlugField(max_length=255, blank=True, null=True)
     icon = models.CharField(max_length=100, null=True, blank=True)
@@ -47,7 +62,9 @@ class Tag(models.Model):
 
 
 class Post(models.Model):
+    """Model for posts"""
     title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, blank=True, null=True)
     category = models.ForeignKey(
         'Category', related_name='category', on_delete=models.CASCADE)
     content = models.TextField(null=True, blank=True)
@@ -55,13 +72,23 @@ class Post(models.Model):
     modified_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
     tag = models.ManyToManyField(Tag)
+    demo_css = models.TextField(null=True, blank=True)
+    demo_js = models.TextField(null=True, blank=True)
+# TODO add related posts to Post model
 
     def __str__(self):
         return self.title
 
+    def __unicode__(self):
+        return self.name
+
     class Meta:
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
 
 admin.site.register(Post)
