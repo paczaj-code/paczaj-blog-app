27033bc164387a89eebf54372c748f6ea34778f6
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-07-28 18:37:39 +0000

    Add views for practise

--- app/practice/views.py
+++ app/practice/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+class PostListAPIViewByCategoryIdOrSlug(APIView):
+    """View for posts list with category data"""
+
+    def get_objects_by_pk(self, pk):
+        try:
+            posts = Post.objects.filter(category_id=pk, is_published=True)
+            category = Category.objects.get(
+                pk=pk, is_enabled=True, category_type='P',)
+            obj = {'posts': posts, 'category': category}
+            return obj
+        except Category.DoesNotExist:
+            raise Http404
+
+    def get_objects_by_slug(self, slug):
+        try:
+            posts = Post.objects.filter(
+                category__slug=slug, is_published=True)
+            category = Category.objects.get(
+                slug=slug, is_enabled=True, category_type='P',)
+            obj = {'posts': posts, 'category': category}
+            return obj
+        except Category.DoesNotExist:
+            raise Http404
