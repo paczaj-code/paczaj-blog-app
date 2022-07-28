6da0adb7d7be6dca246ed4fe0db25d09562cb7fb
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-07-28 18:37:30 +0000

    Add serializers for practise

---
+++ app/practice/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from collections import OrderedDict
+from .models import Practice, Exercise
+
+
+class PractiseListByCategoryId(serializers.ModelSerializer):
+    class Meta:
+        model = Practise
+        fields = ['title', 'slug', 'category']
