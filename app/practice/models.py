cdaa039c16f319a05410cd17a2c30ea9e2506c32
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-07-28 18:37:16 +0000

    Add models for practise

--- app/practice/models.py
+++ app/practice/models.py
@@ -1,38 +1,60 @@
 from django.db import models
+from django.contrib import admin
 from blog.models import Category, Post
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 
 
 class Practice(models.Model):
-    name = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, blank=True, null=True)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
     category = models.ForeignKey(Category,  on_delete=models.CASCADE)
-    exercise = models.TextField()
-    solution = models.TextField()
     related_posts = models.ManyToManyField(Post)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.title
+
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'practice_subject'
+
+
+class Exercise(models.Model):
+    EASY = 'E'
+    MEDIUM = 'M'
+    DIFFICULT = 'D'
+    CHOICES = (
+        (EASY, "Easy"),
+        (MEDIUM, "Medium"),
+        (DIFFICULT, "Difficult"),
+    )
+    title = models.CharField(max_length=255)
+    number = models.IntegerField()
+    practise = models.ForeignKey(Practice, on_delete=models.CASCADE)
+    difficulty = models.CharField(
+        choices=CHOICES, default=MEDIUM, max_length=1)
+    exercise = models.TextField()
+    solution = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
         super().save(*args, **kwargs)
 
     class Meta:
         db_table = 'practice_exercise'
 
+    def __str__(self):
+        return self.title
 
-class UserSolution(models.Model):
-    solution = models.TextField()
-    user = models.ForeignKey(User, related_name='user',
-                             on_delete=models.SET_NULL,  blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
-    practise = models.ForeignKey(
-        'Practice', related_name='practise', on_delete=models.SET_NULL, blank=True, null=True)
 
-    is_public = models.BooleanField(default=False)
-
-    class Meta:
-        db_table = 'practice_user_solution'
+admin.site.register(Exercise)
+admin.site.register(Practice)
