f3724cf59d3d0efc3d91916c557ead81378307ff
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-08-11 08:33:49 +0000

    Add tests for blog models

--- app/blog/test/test_model.py
+++ app/blog/test/test_model.py
@@ -1,63 +1,13 @@
-from django.test import TestCase
 from blog.models import Post, Category, Tag
-
-
-# class CategoryTest(TestCase):
-#     fixtures = ['./fixtures/fixtures.json']
-
-#     def test_inserted_fixtures(self):
-#         self.assertEqual(Category.objects.get(id=1).name, 'Category 1')
-#         self.assertEqual(Category.objects.get(id=1).slug, 'category-1')
-#         self.assertEqual(Category.objects.get(id=1).is_enabled, True)
-#         self.assertEqual(Category.objects.get(id=1).parent, None)
-#         self.assertEqual(Category.objects.get(
-#             id=1).description, "I'm category 1")
-
-#         self.assertEqual(Category.objects.get(id=2).parent.id, 1)
-#         self.assertEqual(Category.objects.get(id=3).is_enabled, False)
-
-#     def test_create_category_withot_name_raises_error(self):
-#         with self.assertRaises(Exception):
-#             Category.objects.create(
-#                 name=None)
-
-#     def test_create_category_with_existing_name_raises_error(self):
-#         with self.assertRaises(Exception):
-#             Category.objects.create(
-#                 name=Category.objects.get(id=1).name)
-
-#     def test_create_category_successfull(self):
-#         Category.objects.create(
-#             name="Category 4", description="I'm category 1", parent=Category.objects.get(id=3))
-
-#         self.assertEqual(Category.objects.all().count(), 4)
-#         self.assertEqual(Category.objects.last().name, 'Category 4')
-#         self.assertEqual(Category.objects.last().slug, 'category-4')
-#         self.assertEqual(Category.objects.last(
-#         ).description, "I'm category 1")
-#         self.assertEqual(Category.objects.last().parent.id, 3)
-
-#     def test_update_category_successfull(self):
-#         category = Category.objects.first()
-#         category.name = 'New category'
-#         category.description = 'New description'
-#         category.is_enabled = False
-#         category.parent = Category.objects.get(id=2)
-#         category.save()
-#         self.assertEqual(Category.objects.first().name, 'New category')
-#         self.assertEqual(Category.objects.first().slug, 'new-category')
-#         self.assertEqual(Category.objects.first().description,
-#                          'New description')
-#         self.assertEqual(Category.objects.first().is_enabled, False)
-#         self.assertEqual(Category.objects.first().parent.id, 2)
-
-#     def test_delete_category(self):
-#         Category.objects.last().delete()
-#         self.assertEqual(Category.objects.all().count(), 2)
+from django.test import TestCase
+from django.core.management import call_command
 
 
 class TagTest(TestCase):
-    fixtures = ['./fixtures/fixtures.json']
+    """Tests for tag model"""
+    @classmethod
+    def setUpTestData(cls):
+        call_command('loaddata', 'fixtures/fixtures_new.json', verbosity=0)
 
     def test_inserted_fixtures(self):
         self.assertEqual(Tag.objects.all().count(), 3)
@@ -100,17 +50,20 @@ class TagTest(TestCase):
 
 
 class PostTest(TestCase):
-    fixtures = ['./fixtures/fixtures.json']
+    """Tests for post model"""
+    @classmethod
+    def setUpTestData(cls):
+        call_command('loaddata', 'fixtures/fixtures_new.json', verbosity=0)
 
     def test_inserted_fixtures(self):
         self.assertEqual(Post.objects.all().count(), 3)
         self.assertEqual(Post.objects.get(id=1).title, 'Post 1 title')
+        self.assertEqual(Post.objects.get(id=1).slug, 'post-1-title')
         self.assertEqual(Post.objects.get(id=1).content, 'Post 1 content')
         self.assertEqual(Post.objects.get(id=1).is_published, True)
         self.assertEqual(Post.objects.get(id=1).category.id, 1)
         self.assertEqual(Post.objects.get(id=1).tag.first().id, 1)
         self.assertEqual(Post.objects.get(id=1).tag.count(), 1)
-    # TODO testy dla powiązanycg ćwiczeń
 
     def test_create_post_with_without_title_raises_error(self):
         with self.assertRaises(Exception):
@@ -120,17 +73,19 @@ class PostTest(TestCase):
     def test_post_created_successfull(self):
         Post.objects.create(title='New title',
                             content='New content', category=Category.objects.get(id=3))
-    #
         Post.objects.last().tag.add(Tag.objects.last())
         Post.objects.last().tag.add(Tag.objects.first())
+        Post.objects.last().related_articles.add(Post.objects.get(id=1))
 
         self.assertEqual(Post.objects.all().count(), 4)
         self.assertEqual(Post.objects.last().title, 'New title')
+        self.assertEqual(Post.objects.last().slug, 'new-title')
         self.assertEqual(Post.objects.last().content, 'New content')
         self.assertEqual(Post.objects.last().category.id, 3)
         self.assertEqual(Post.objects.last().tag.count(), 2)
         self.assertEqual(Post.objects.last().tag.first().id, 1)
         self.assertEqual(Post.objects.last().tag.last().id, 3)
+        self.assertEqual(Post.objects.last().related_articles.last().id, 1)
 
     def test_update_post_success(self):
         post = Post.objects.last()
@@ -141,16 +96,19 @@ class PostTest(TestCase):
         post.tag.remove(Tag.objects.get(id=1))
         post.tag.add(Tag.objects.get(id=2))
         post.tag.add(Tag.objects.get(id=3))
+        post.related_articles.remove(Post.objects.get(id=1))
         post.save()
         self.assertEqual(Post.objects.last().title, 'Post 55')
+        self.assertEqual(Post.objects.last().slug, 'post-55')
         self.assertEqual(Post.objects.last().content, '<p>paragraph</p>')
         self.assertEqual(Post.objects.last().is_published, False)
         self.assertEqual(Post.objects.last().category.id, 3)
         self.assertEqual(Post.objects.last().tag.count(), 2)
+        self.assertEqual(Post.objects.last().related_articles.count(), 1)
 
     def test_delete_post_successfull(self):
         Post.objects.last().delete()
         self.assertEqual(Post.objects.all().count(), 2)
 
 
-# TODO dodać slug i powiązane ćwiczenia do post
+# # TODO dodać slug i powiązane ćwiczenia do post
