b398cbbb4f1ae4b73a2d3dc29b9b01a71a82364b
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-08-14 14:19:05 +0000

    Add serializers for details endpoints

---
+++ app/category/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from collections import OrderedDict
+from .models import Category
+from blog.models import Post
+
+
+class BasicCategorySerializer(serializers.ModelSerializer):
+    """Serializer template for categories to be inherited"""
+    posts = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'icon', 'slug', 'posts']
+
+    def to_representation(self, instance):
+        """Clean of empty values and add count of subcategories"""
+        result = super(BasicCategorySerializer,
+                       self).to_representation(instance)
+        result['subcount'] = instance.subcategories.count()
+        return OrderedDict([(key, result[key]) for key in result if result[key] is not None and result[key] != []])
+
+    def get_posts(self, obj):
+        """Add field post as count of related articles"""
+        posts = Post.objects.filter(category_id__in=[obj]).count()
+        return posts
+
+
+class PublicCategoryListSerializer(BasicCategorySerializer):
+    """Serilazer for category list API with recursive subcategories"""
+    class Meta:
+        model = Category
+        fields = BasicCategorySerializer.Meta.fields
+
+    def get_fields(self):
+        fields = super(PublicCategoryListSerializer, self).get_fields()
+        fields['subcategories'] = PublicCategoryListSerializer(many=True)
+        return fields
+
+
+class PublicCategoryDetailSerializer(BasicCategorySerializer):
+    """Serializer for category detail API """
+    class Meta:
+        model = Category
+        fields = BasicCategorySerializer.Meta.fields + ['description']
