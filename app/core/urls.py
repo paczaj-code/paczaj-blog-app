1fda60221225e2ad00d92c6e3a83f3d363b7448d
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-08-15 09:57:22 +0000

    Add urls for blog app

--- app/core/urls.py
+++ app/core/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from terminology import urls
 from blog import urls
+from category import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('category.urls')),
+    path('api/', include('blog.urls')),
     # path('api/', include('terminology.urls')),
     # path('api/', include('blog.urls')),
 ]
