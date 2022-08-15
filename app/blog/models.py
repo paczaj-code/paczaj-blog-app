2af9928a60a473c774e46fe368be3c5255daa20b
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-08-15 20:27:44 +0000

    Add image fields

--- app/blog/models.py
+++ app/blog/models.py
@@ -1,8 +1,9 @@
-from distutils import text_file
 from django.db import models
 from django.contrib import admin
 from category.models import Category
 from django.template.defaultfilters import slugify
+import json
+import requests
 
 
 class Tag(models.Model):
@@ -41,6 +42,8 @@ class Post(models.Model):
     demo_css = models.TextField(null=True, blank=True)
     demo_js = models.TextField(null=True, blank=True)
     related_posts = models.ManyToManyField('Post', blank=True)
+    image_id = models.CharField(max_length=255, blank=True, null=True)
+    image = models.JSONField(blank=True, null=True)
 
     def __str__(self):
         return self.title
@@ -52,6 +55,13 @@ class Post(models.Model):
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
+        if self.image_id:
+            result = requests.get(
+                f'https://api.unsplash.com/photos/{self.image_id}?client_id=eWnEhgP8URLT8IwJXSsslgayoKT2vkltpdJtfu-vrV8')
+            image_data = result.json()
+            image_dict = {
+                "id": image_data["id"], "urls": image_data["urls"], "user": image_data["user"]}
+            self.image = json.dumps(image_dict)
         super().save(*args, **kwargs)
 
 
--- app/blog/serializers.py
+++ app/blog/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from collections import OrderedDict
 from blog.models import Tag, Post
 from category.serializers import PublicCategoryDetailSerializer
-
+import json
 
 """-----------------Tag serializers begin----------------------"""
 
@@ -25,16 +25,33 @@ class TagSerializer(serializers.ModelSerializer):
 """-----------------Post serializers begin ----------------------"""
 
 
+class JSONSerializerField(serializers.Field):
+    """Serializer for JSONField -- required to make field writable"""
+
+    def to_representation(self, value):
+        json_data = {}
+        try:
+            json_data = json.loads(value)
+        except ValueError as e:
+            raise e
+        finally:
+            return json_data
+
+    def to_internal_value(self, data):
+        return json.dumps(data)
+
+
 class BasicPostSerializer(serializers.ModelSerializer):
     """Serializer template for post to be inherited"""
     created_at = serializers.DateTimeField(format='%d-%m-%Y')
     modified_at = serializers.DateTimeField(format='%d-%m-%Y')
     tag = TagSerializer(many=True, read_only=True)
+    image = JSONSerializerField()
 
     class Meta:
         model = Post
         fields = ['id', 'title', 'slug', 'created_at',
-                  'modified_at', 'tag']
+                  'modified_at', 'tag', 'image']
 
     def to_representation(self, instance):
         """Clean of empty values and add count of subcategories"""
