699c2fb10e82008058fd98e09bdf8df550aa2d8b
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-08-15 18:18:43 +0000

    Add urls for terminology app

--- app/terminology/urls.py
+++ app/terminology/urls.py
@@ -1,12 +1,12 @@
-# from django.urls import path
-# from .views import TermListAPIView, TermDetailAPIViewByPK, TermDetailAPIViewBySlug
+from django.urls import path
+from .views import TermListAPIView, TermDetailAPIViewByPK, TermDetailAPIViewBySlug
 
 
-# urlpatterns = [
-#     path('term/', TermListAPIView.as_view(), name='term-list'),
-#     path('term/<int:pk>/', TermDetailAPIViewByPK.as_view(), name='term-detail-id'),
-#     path('term/<str:slug>/', TermDetailAPIViewBySlug.as_view(),
-#          name='term-detail-slug'),
+urlpatterns = [
+    path('term/', TermListAPIView.as_view(), name='term-list'),
+    path('term/<int:pk>/', TermDetailAPIViewByPK.as_view(), name='term-detail-id'),
+    path('term/<str:slug>/', TermDetailAPIViewBySlug.as_view(),
+         name='term-detail-slug'),
 
-# ]
-# app_name = 'terminology'
+]
+app_name = 'terminology'
