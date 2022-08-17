8d0b41425f416db79bb2ef3a688d23372f4e47fe
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-08-17 07:04:29 +0000

    Correct model

--- app/practice/models.py
+++ app/practice/models.py
@@ -9,7 +9,7 @@ from django.db.models import Max
 
 class Practice(models.Model):
     title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255)
+    slug = models.SlugField(max_length=255, null=True, blank=True)
     category = models.ForeignKey(Category,  on_delete=models.CASCADE)
     related_posts = models.ManyToManyField(Post)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -38,7 +38,7 @@ class Exercise(models.Model):
         (DIFFICULT, "Difficult"),
     )
     # title = models.CharField(max_length=255)
-    number = models.IntegerField()
+    number = models.IntegerField(blank=True)
     practise = models.ForeignKey(Practice, on_delete=models.CASCADE)
     difficulty = models.CharField(
         choices=CHOICES, default=MEDIUM, max_length=1)
@@ -48,23 +48,26 @@ class Exercise(models.Model):
     modified_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
 
-    # def save(self, *args, **kwargs):
-    #     last_number = Exercise.objects.filter(
-    #         practise=self.practise).aggregate(Max(number))
+    def save(self, *args, **kwargs):
+        already_created = Exercise.objects.filter(pk=self.pk).exists()
+        last_number = Exercise.objects.filter(
+            practise=self.practise).values('number').order_by('-number').first()
 
-    #     if last_number is None:
-    #         number = 1
-    #     else:
-    #         number = last_number + 1
-    #     # self.slug = slugify(self.title)
-    #     super().save(*args, **kwargs)
+        if already_created:
+            self.number = self.number
+
+        elif last_number and already_created == False:
+            self.number = last_number['number'] + 1
+        else:
+            self.number = 1
+        super().save(*args, **kwargs)
 
     class Meta:
         db_table = 'exercise'
 
     def __str__(self):
-        return self.title
+        return f'{self.practise}-{self.number}'
 
 
-# admin.site.register(Exercise)
-# admin.site.register(Practice)
+admin.site.register(Exercise)
+admin.site.register(Practice)
