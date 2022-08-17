1967366877bdf1dfc4ded017d7dab47328a4f6c0
Marcin Paczkowski <marcinpaczkowski@icloud.com>
2022-08-17 07:04:50 +0000

    Serialzers for Practise all

--- app/practice/serializers.py
+++ app/practice/serializers.py
@@ -1,9 +1,70 @@
+from dataclasses import fields
 from rest_framework import serializers
 from collections import OrderedDict
-# from .models import Practice, Exercise
+from .models import Practice, Exercise
+from category.serializers import PublicCategoryDetailSerializer
+from blog.models import Post
 
 
-# class PractiseListByCategoryId(serializers.ModelSerializer):
-#     class Meta:
-#         model = Practise
-#         fields = ['title', 'slug', 'category']
+class RelatedPostsSerializer(serializers.ModelSerializer):
+    """Tag serializer for Post serializers"""
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'slug']
+
+
+class RelatedExercises(serializers.ModelSerializer):
+    """Serializer for"""
+    class Meta:
+        model = Exercise
+        fields = ['id', 'number', 'difficulty']
+
+
+class BasePractiseSerializer(serializers.ModelSerializer):
+    num_exer = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Practice
+        # fields = ['id', 'title', 'slug', 'category',
+        #           'modified_at', 'created_at']
+        fields = ['id', 'title', 'slug',
+                  'created_at', 'modified_at', 'num_exer']
+
+    def get_num_exer(self, obj):
+        exer = Exercise.objects.filter(practise=obj).count()
+        return exer
+
+
+class PractiseListByCategoryIdOrSlug(serializers.Serializer):
+    """Serialzer for post list by category id or slug with category data"""
+    practice = BasePractiseSerializer(many=True)
+    category = PublicCategoryDetailSerializer(read_only=True)
+
+    class Meta:
+        fields = ['practice', 'category']
+
+
+class PractiseDetailByIdOrSlug(serializers.ModelSerializer):
+    related_posts = RelatedPostsSerializer(many=True, read_only=True)
+    # related_exer = RelatedExercises(many=True, read_only=True)
+    related_exer = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Practice
+        fields = ['id', 'title', 'created_at', 'modified_at',
+                  'related_posts', 'related_exer']
+
+    def get_related_exer(self, obj):
+        rel = Exercise.objects.filter(practise_id=obj)
+        # return rel
+        return RelatedExercises(rel, many=True).data
+
+
+class ExersiceSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Exercise
+        fields = ['id', 'number', 'difficulty', 'exercise', 'solution']
+
+
+# TODO Refactoring i dodac daty i testy
